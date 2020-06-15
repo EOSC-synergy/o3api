b7258dcd00aee7890a46b959a415435a4d75c876
valentin.kozlov <valentin.kozlov@kit.edu>
2020-06-15 12:40:01 +0200

    update example script

--- udocker_job_example.sh
+++ udocker_job_example.sh
@@ -10,9 +10,9 @@
 
 #### if udocker is not installed, do:
 # curl https://raw.githubusercontent.com/indigo-dc/udocker/master/udocker.py > udocker
-# mv udocker $HOME/.local/bin (or something in your PATH)
 # chmod u+rx ./udocker
-# ./udocker install
+# mv udocker $HOME/.local/bin # (or something in your PATH)
+# udocker install
 ##
 # More INFO: https://github.com/indigo-dc/udocker/blob/master/doc/installation_manual.md
 ####
@@ -20,15 +20,17 @@
 UDOCKER_CONTAINER="o3as-ubuntu"
 
 ##### If you need to pull NEW image and CREATE Container, do first:
-#udocker pull synergyimk/o3as:ubuntu
-#udocker create --name=o3as-ubuntu synergyimk/o3as:ubuntu
+udocker pull synergyimk/o3as:ubuntu
+udocker create --name=o3as-ubuntu synergyimk/o3as:ubuntu
 # NB: creating container may take 5-10 minutes...
+#may change execmode:
+#udocker setup --execmode=F3 o3as-ubuntu
 #####
 
 HOST_DATA_RAW=$HOME/synergy/o3as/Data/raw/Ecmwf
 HOST_DATA_OUT=$HOME/synergy/o3as/Data/output
 
 UDOCKER_OPTIONS="-v $HOST_DATA_RAW:/mnt/data/input -v $HOST_DATA_OUT:/mnt/data/output"
-UDOCKER_RUN_COMMAND="/srv/o3as/eosc.sh -i /mnt/data/input/ -o /mnt/data/output/ -b 1980 -e 1982 -l 50"
+UDOCKER_RUN_COMMAND="/srv/o3as/eosc.sh -i /mnt/data/input/ -o /mnt/data/output/ -b 1980 -e 1981 -l 50"
 
 udocker run  ${UDOCKER_OPTIONS} ${UDOCKER_CONTAINER} ${UDOCKER_RUN_COMMAND}
